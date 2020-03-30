bfae86843178e1c7ae793dda8f42ad2bf56ccde3
Arache Abderrahmane <46249965+Abderrahmanearache@users.noreply.github.com>
2020-03-30 11:18:18 +0100

    Update Json.java

--- src/main/java/ApiConsumer/json/Json.java
+++ src/main/java/ApiConsumer/json/Json.java
@@ -1,92 +1,98 @@
 package ApiConsumer.json;
-
+ 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Json {
+    private static final Set<Class> WRAPPER_TYPES = new HashSet(Arrays.asList(Boolean.class, Character.class, Byte.class, Short.class, Integer.class, Long.class, Float.class, Double.class, Void.class));
 
-    private static final Set<Class> WRAPPER_TYPES = new HashSet<>(Arrays.asList(
-            Boolean.class,
-            Character.class,
-            Byte.class,
-            Short.class,
-            Integer.class,
-            Long.class,
-            Float.class,
-            Double.class,
-            Void.class)
-    );
+    public Json() {
+    }
 
     public static String from(Object obj) throws JsonFormatingException {
-
         String json = "";
         boolean isArray = obj.getClass().isArray();
+        boolean isList = obj instanceof List;
+        if (isArray)
+            for (int i = 0; i < Array.getLength(obj); ++i) {
+                Object inst = Array.get(obj, i);
+                if (inst != null) {
+                    String format;
+                    if (isPrimitive(inst))
+                        format = String.format("{ \"%d\" : %s }", i, inst);
+                    else if (inst instanceof CharSequence)
+                        format = String.format("{ \"%d\" : \"%s\" }", i, inst);
+                    else
+                        format = from(inst);
 
-        if (isArray) {
-            for (int i = 0; i < Array.getLength(obj); i++) {
-                String format;
-
-                if (Array.get(obj, i) == null)
-                    continue;
-                else if (isPrimitive(Array.get(obj, i)))
-                    format = String.format("{ \"%d\" : %s }", i, Array.get(obj, i));
-                else if (Array.get(obj, i) instanceof CharSequence)
-                    format = String.format("{ \"%d\" : \"%s\" }", i, Array.get(obj, i));
-                else
-                    format = from(Array.get(obj, i));
-
-                json += !json.isEmpty() ? "," + format : format;
+                    json = json + (!json.isEmpty() ? "," + format : format);
+                }
             }
-        } else
-            try {
-                for (Field declaredField : obj.getClass().getDeclaredFields()) {
-                    String s = jsonField(obj, declaredField);
+        else if (isList) {
+            List list = (List) obj;
+            for (int i = 0; i < list.size(); ++i) {
+                Object o = list.get(i);
+                if (o != null) {
+                    String format;
+                    if (isPrimitive(o))
+                        format = String.format("{ \"%d\" : %s }", i, o);
+                    else if (o instanceof CharSequence)
+                        format = String.format("{ \"%d\" : \"%s\" }", i, o);
+                    else
+                        format = from(o);
 
-                    json += json.length() == 0 ? s : " , " + s;
+                    json = json + (!json.isEmpty() ? "," + format : format);
+                }
+            }
+        } else {
+            try {
+                Field[] fields = obj.getClass().getDeclaredFields();
+
+                for (int i = 0; i < fields.length; ++i) {
+                    Field declaredField = fields[i];
+                    if (declaredField.isSynthetic())
+                        continue;
+                    String fieldString = jsonField(obj, declaredField);
+                    json = json + (json.length() == 0 ? fieldString : " , " + fieldString);
                 }
             } catch (Exception e) {
                 throw new JsonFormatingException(e);
             }
-        if (isArray)
+        }
+
+        if (isArray || isList) {
             json = "[" + json + "]";
-        else
+        } else {
             json = "{" + json + "}";
+        }
 
         return json.replaceAll("\\s+", " ");
     }
 
-    private static String jsonField(Object object, Field field) throws JsonFormatingException {
-        String str;
-
-        if (!field.isAccessible())
+    private static String jsonField(Object object, Field field) throws Exception {
+        if (!field.isAccessible()) {
             field.setAccessible(true);
-
-        Object obj;
-        try {
-            obj = field.get(object);
-        } catch (IllegalAccessException e) {
-            throw new JsonFormatingException(e);
         }
 
-        if (obj == null)
-            str = String.format(" \"%s\" : %s ", field.getName(), obj);
-        else if (isPrimitive(obj))
-            str = String.format(" \"%s\" : %s ", field.getName(), obj);
-        else if (obj instanceof CharSequence)
-            str = String.format(" \"%s\" : \"%s\" ", field.getName(), obj);
-        else
-            str = String.format(" \"%s\" : %s ", field.getName(), from(obj));
+        Object obj = field.get(object);
 
-        return str;
+
+        if (obj == null)
+            return String.format(" \"%s\" : %s ", field.getName(), obj);
+        else if (isPrimitive(obj))
+            return String.format(" \"%s\" : %s ", field.getName(), obj);
+        else if (obj instanceof CharSequence)
+            return String.format(" \"%s\" : \"%s\" ", field.getName(), obj);
+        else
+            return String.format(" \"%s\" : %s ", field.getName(), from(obj));
 
     }
 
     private static boolean isPrimitive(Object clazz) {
         return WRAPPER_TYPES.contains(clazz.getClass());
     }
-
-
 }
